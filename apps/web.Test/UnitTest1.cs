25523f64b81f8c2ff6a967c855dc919327690b8c
ZeekoZhu <vaezt@outlook.com>
2023-01-14 16:40:59 +0800

    style: fix code style

--- apps/web.Test/UnitTest1.cs
+++ apps/web.Test/UnitTest1.cs
@@ -2,9 +2,9 @@ namespace AurPackager.Web.Test;
 
 public class UnitTest1
 {
-    [Fact]
-    public void Test1()
-    {
+  [Fact]
+  public void Test1()
+  {
 
-    }
+  }
 }
--- apps/web/Migrations/20230109002630_InitialCreate.cs
+++ apps/web/Migrations/20230109002630_InitialCreate.cs
@@ -1,33 +1,33 @@
-﻿using Microsoft.EntityFrameworkCore.Migrations;
+using Microsoft.EntityFrameworkCore.Migrations;
 
 #nullable disable
 
 namespace AurPackager.Web.Migrations
 {
+  /// <inheritdoc />
+  public partial class InitialCreate : Migration
+  {
     /// <inheritdoc />
-    public partial class InitialCreate : Migration
+    protected override void Up(MigrationBuilder migrationBuilder)
     {
-        /// <inheritdoc />
-        protected override void Up(MigrationBuilder migrationBuilder)
-        {
-            migrationBuilder.CreateTable(
-                name: "AurPackages",
-                columns: table => new
-                {
-                    PackageName = table.Column<string>(type: "TEXT", nullable: false),
-                    LocalRepoName = table.Column<string>(type: "TEXT", nullable: false)
-                },
-                constraints: table =>
-                {
-                    table.PrimaryKey("PK_AurPackages", x => x.PackageName);
-                });
-        }
-
-        /// <inheritdoc />
-        protected override void Down(MigrationBuilder migrationBuilder)
-        {
-            migrationBuilder.DropTable(
-                name: "AurPackages");
-        }
+      migrationBuilder.CreateTable(
+          name: "AurPackages",
+          columns: table => new
+          {
+            PackageName = table.Column<string>(type: "TEXT", nullable: false),
+            LocalRepoName = table.Column<string>(type: "TEXT", nullable: false)
+          },
+          constraints: table =>
+          {
+            table.PrimaryKey("PK_AurPackages", x => x.PackageName);
+          });
     }
+
+    /// <inheritdoc />
+    protected override void Down(MigrationBuilder migrationBuilder)
+    {
+      migrationBuilder.DropTable(
+          name: "AurPackages");
+    }
+  }
 }
